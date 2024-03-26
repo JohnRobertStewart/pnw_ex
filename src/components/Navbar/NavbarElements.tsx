92a598b5c4446dcc9677bff93e456095cfa22516
Johnny <johnrobertstewart@protonmail.com>
2024-03-26 12:27:16 -0600

    fixing compat

--- src/components/Navbar/NavbarElements.tsx
+++ src/components/Navbar/NavbarElements.tsx
@@ -47,9 +47,7 @@ export const NavMenu = styled.div`
     /* Third Nav */
     /* width: 100vw;
 white-space: nowrap; */
-    @media screen and (max-width: 768px) {
-        display: all;
-    }
+
     
 `;
 
